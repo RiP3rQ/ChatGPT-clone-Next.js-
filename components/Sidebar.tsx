bc5934cfe65c1dc0d316f619dde193a0128e370b
RiP3rQ <rafalpompa2000@gmail.com>
2023-02-07 20:32:59 +0100

    logout done

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,11 @@
+"use client";
+
+import { useSession, signOut } from "next-auth/react";
 import NewChat from "./NewChat";
 
 function Sidebar() {
+  const { data: session } = useSession();
+
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
@@ -13,6 +18,15 @@ function Sidebar() {
         {/* Chat list - map through them */}
         <div>{/* chats */}</div>
       </div>
+
+      {session && (
+        <img
+          src={session.user?.image!}
+          alt="Profile Picture"
+          className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+          onClick={() => signOut()}
+        />
+      )}
     </div>
   );
 }
