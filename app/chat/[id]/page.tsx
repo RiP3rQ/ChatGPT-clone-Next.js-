15ae48032c4f7c5871738ce4c514c9574c4b7824
RiP3rQ <rafalpompa2000@gmail.com>
2023-02-08 18:49:17 +0100

    ChatInput component

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Chat from "../../../components/Chat";
+import ChatInput from "../../../components/ChatInput";
 
-function ChatPage() {
-  return <div> ChatPage</div>;
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+function ChatPage({ params: { id } }: Props) {
+  return (
+    <div className="flex flex-col h-screen overflow-hidden">
+      {/* Chat */}
+      <Chat chatId={id} />
+      {/* ChatInput */}
+      <ChatInput chatId={id} />
+    </div>
+  );
 }
 
 export default ChatPage;
---
+++ components/Chat.tsx
@@ -0,0 +1,9 @@
+type Props = {
+  chatId: string;
+};
+
+function Chat({ chatId }: Props) {
+  return <div>Chat</div>;
+}
+
+export default Chat;
