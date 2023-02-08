2040577ddfd08d60cbf7a16f088f0bc45f5036db
RiP3rQ <rafalpompa2000@gmail.com>
2023-02-08 19:48:45 +0100

    api working with firebase admin

--- pages/api/askQuestion.ts
+++ pages/api/askQuestion.ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import query from "../../lib/queryApi";
+import admin from "firebase-admin";
+import { adminDb } from "../../firebaseAdmin";
 
 type Data = {
   answer: string;
@@ -28,8 +30,21 @@ export default async function handler(
 
   const message: Message = {
     text: response || "ChatGPT wasn't unable to find an answer for that!",
-    // dokończyć
+    createdAt: admin.firestore.Timestamp.now(),
+    user: {
+      _id: "ChatGPT",
+      name: "ChatGPT",
+      avatar: "https://links.papareact.com/89k",
+    },
   };
 
-  res.status(200).json({ answer: "John Doe" });
+  await adminDb
+    .collection("users")
+    .doc(session?.user?.email)
+    .collection("chats")
+    .doc(chatId)
+    .collection("messages")
+    .add(message);
+
+  res.status(200).json({ answer: message.text });
 }
