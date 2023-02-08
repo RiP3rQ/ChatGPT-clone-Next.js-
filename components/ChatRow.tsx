82a3e45c03741aaf4d58526d5d70e94e32e27feb
RiP3rQ <rafalpompa2000@gmail.com>
2023-02-08 18:28:11 +0100

    ChatRow elements

---
+++ components/ChatRow.tsx
@@ -0,0 +1,44 @@
+import { ChatBubbleLeftIcon, TrashIcon } from "@heroicons/react/24/solid";
+import { collection, orderBy, query } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+
+type Props = {
+  id: string;
+};
+
+function ChatRow({ id }: Props) {
+  const pathname = usePathname();
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [active, setActive] = useState<boolean>(false);
+
+  const [messages] = useCollection(
+    collection(db, "users", session?.user?.email!, "chats", id, "messages")
+  );
+
+  useEffect(() => {
+    if (!pathname) return;
+
+    setActive(pathname.includes(id));
+  }, [pathname]);
+
+  return (
+    <Link
+      href={`/chat/${id}`}
+      className={`chatRow justify-center ${active && "bg-gray-700/50"}`}
+    >
+      <ChatBubbleLeftIcon className="h5 w-5" />
+      <p className="flex-1 hidden md:inline-flex truncate">
+        {messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"}
+      </p>
+      <TrashIcon className="h5 w-5 text-gray-700 hover:text-red-700" />
+    </Link>
+  );
+}
+
+export default ChatRow;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,9 +2,21 @@
 
 import { useSession, signOut } from "next-auth/react";
 import NewChat from "./NewChat";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { collection, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
+import ChatRow from "./ChatRow";
 
 function Sidebar() {
   const { data: session } = useSession();
+
+  const [chats, loading, error] = useCollection(
+    session &&
+      query(
+        collection(db, "users", session.user?.email!, "chats"),
+        orderBy("createdAt", "asc")
+      )
+  );
 
   return (
     <div className="p-2 flex flex-col h-screen">
@@ -16,7 +28,11 @@ function Sidebar() {
         {/* ModelSelection */}
         <div>{/* model */}</div>
         {/* Chat list - map through them */}
-        <div>{/* chats */}</div>
+        <div>
+          {chats?.docs.map((chat) => (
+            <ChatRow key={chat.id} id={chat.id} />
+          ))}
+        </div>
       </div>
 
       {session && (
